50294db54deb56d9aed40d4413cdcbc74add9c88
Steven Slack <steven@s2webpress.com>
2015-05-18 16:28:52 -0400

    Removed emoji support

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -19,7 +19,8 @@ if ( ! defined( 'DISALLOW_FILE_MODS' ) ) {
  * Clean Up WP HEAD
  */
 function head_cleanup() {
-
+	// remove feed links
+	remove_action('wp_head', 'feed_links_extra', 3);
 	// EditURI link
 	remove_action( 'wp_head', 'rsd_link' );
 	// windows live writer
@@ -33,6 +34,15 @@ function head_cleanup() {
 	// WP version
 	remove_action( 'wp_head', 'wp_generator' );
 
+	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+
 } /* end head cleanup */
 add_action( 'init', __NAMESPACE__ . '\\head_cleanup' );
 
@@ -43,7 +53,7 @@ add_action( 'init', __NAMESPACE__ . '\\head_cleanup' );
 function remove_admin_bar_links() {
 
     global $wp_admin_bar;
-    
+
     $wp_admin_bar->remove_menu('wp-logo');          // Remove the WordPress logo
     $wp_admin_bar->remove_menu('about');            // Remove the about WordPress link
     $wp_admin_bar->remove_menu('wporg');            // Remove the WordPress.org link
