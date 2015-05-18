0bc6b4dc697c1cb27b7322372e8eec71c6be0e7d
Steven Slack <steven@s2webpress.com>
2015-05-18 17:59:55 -0400

    Added support for WordPress customizer

---
+++ assets/scripts/customizer.js
@@ -0,0 +1,15 @@
+/**
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+
+( function( $ ) {
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.brand' ).text( to );
+		} );
+	} );
+	
+} )( jQuery );
